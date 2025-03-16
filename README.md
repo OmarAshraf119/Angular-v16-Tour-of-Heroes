c41eb9e5e8302a4ccbcb8c936043d57392623b72
OmarAshraf119 <git@stackblitz.com>
2025-03-16 21:15:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-v16-Tour-of-Heroes
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/OmarAshraf119/Angular-v16-Tour-of-Heroes)
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule, AppRoutingModule,    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
