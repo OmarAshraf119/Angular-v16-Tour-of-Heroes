21ab08250e0386026ba5ca84843d432dea6cf4e8
OmarAshraf119 <omarashraf11918@gmail.com>
2025-03-16 22:01:35 +0200

    Http Added with Mock database

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Angular-v16-Tour-of-Heroes
+# Tour-of-heroes-http
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/OmarAshraf119/Angular-v16-Tour-of-Heroes)
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/OmarAshraf119/Tour-of-heroes-http)
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -9,5 +9,12 @@ export const HEROES: Hero[] = [
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr. IQ' },
   { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 20, name: 'Tornado' }
 ];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
